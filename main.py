9582730c32d6a6ae8e1db2dddbf76b965d8c6f15
Taylor Willmott <contact@taylorwillmott.com>
2020-11-11 19:48:09 +0000

    Added help text to commands list and a 'help' command to display it

--- main.py
+++ main.py
@@ -17,8 +17,17 @@ async def on_ready():
     print(f'Logged in as {client.user.name}#{client.user.discriminator} ({client.user.id})')
 
 commands = {
-  'rtd': utility.rtd,
+  'rtd': [utility.rtd, 'Roll the dice. Will you be lucky?'],
 }
+
+async def bothelp(message, args):
+  embed = discord.Embed(title = f'{client.user.name} Commands', description = '')
+  embed.set_author(name=f'My prefix is currently set to: {config["prefix"]}')
+  for command in commands:
+    embed.description += f'{config["prefix"]}{command} - {commands[command][1]}\n'
+  await message.channel.send(embed=embed)
+
+commands['help'] = [bothelp, "You're looking at it!"]
 
 @client.event
 async def on_message(message):
@@ -29,7 +38,7 @@ async def on_message(message):
       if cmd == f'{config["prefix"]}{command}':
         await message.add_reaction('👍')
         args = splitmessage[1:]
-        await commands[command](message, args)
+        await commands[command][0](message, args)
         break
   elif message.content.startswith(f'<@!{client.user.id}>'):
     splitmessage = message.content.split()
@@ -38,7 +47,7 @@ async def on_message(message):
       if cmd == command:
         await message.add_reaction('👍')
         args = splitmessage[2:]
-        await commands[command](message, args)
+        await commands[command][0](message, args)
         break
 
 client.run(os.environ['TOKEN'])
