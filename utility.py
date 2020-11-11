e978e3e75f9dbaf2b2b89c09dc44eb424756048a
Taylor Willmott <contact@taylorwillmott.com>
2020-11-11 18:21:47 +0000

    utility.rtd now returns an emoji if a suitable one exists

--- utility.py
+++ utility.py
@@ -1,9 +1,14 @@
 from random import randint
 
 async def rtd(message, args):
+  numbers = ['one','two','three','four','five','six','seven','eight','nine','ten']
   try:
     limit = int(args[0])
   except:
     limit = 6
   result = randint(1,limit)
   await message.channel.send(content = f'You rolled a {str(result)}')
+  if result < len(numbers):
+    await message.channel.send(content = f'You rolled a :{numbers[result-1]}:')
+  else:
+    await message.channel.send(content = f'You rolled a {str(result)}')
